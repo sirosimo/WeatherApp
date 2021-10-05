642ced3d322db23bb84be0036212926e9732038e
sirosimoCCAM <89484611+sirosimoCCAM@users.noreply.github.com>
2021-10-05 10:17:29 -0400

    Add project files.

---
+++ WeatherApp/ViewModels/Helpers/AccuWeatherHelper.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Models;
+
+namespace WeatherApp.ViewModels
+{
+    public class AccuWeatherHelper
+    {
+        private const string BASE_URL = "http://dataservice.accuweather.com/";
+        private const string AUTOCOMPLETE_ENDPOINT = "locations/v1/cities/autocomplete?apikey={0}&q={1}";
+        private const string CURRENT_ENDPOINT = "currentconditions/v1/{0}?apikey={1}";
+        private const string API_KEY = "T6ly1ieSTZ52KETWiBx65Ep3tcPHOnOg";
+
+        public static async Task<List<City>> GetCities(string query)
+        {
+            List<City> cities = new List<City>();
+            string url = BASE_URL + String.Format(AUTOCOMPLETE_ENDPOINT, API_KEY, query);
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+
+                cities = JsonConvert.DeserializeObject<List<City>>(json);
+            }
+
+            return cities;
+        }
+
+        public static async Task<CurrentConditions> GetCurrentWeather(string cityKey)
+        {
+            CurrentConditions currentWeather = new CurrentConditions();
+            string url = BASE_URL + String.Format(CURRENT_ENDPOINT, cityKey, API_KEY);
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+
+                currentWeather = JsonConvert.DeserializeObject<List<CurrentConditions>>(json).FirstOrDefault();
+            }
+
+            return currentWeather;
+        }
+
+        
+
+    }
+}
---
+++ WeatherApp/ViewModels/WeatherViewModel.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Models;
+using WeatherApp.ViewModels.Commands;
+
+namespace WeatherApp.ViewModels
+{
+    public class WeatherViewModel : INotifyPropertyChanged
+    {
+        private string _query;
+
+        public string Query
+        {
+            get { return _query; }
+            set {
+                _query = value;
+                OnPropertyChange("Query");
+            }
+        }
+
+        public ObservableCollection<City> Cities { get; set; }
+
+        private CurrentConditions _currentConditions;
+
+        public CurrentConditions CurrentConditions
+        {
+            get { return _currentConditions;  }
+            set { 
+                _currentConditions = value;
+                OnPropertyChange("CurrentConditions");
+            }
+        }
+
+        private City _selectedCity;
+
+        public City SelectedCity
+        {
+            get { return _selectedCity; }
+            set {
+                _selectedCity = value;
+                OnPropertyChange("SelectedCity");
+                GetCurrentConditions();
+            }
+        }
+
+        public SearchCommand SearchCommand { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public WeatherViewModel()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            {
+                SelectedCity = new City()
+                {
+                    LocalizedName = "Richmond"
+                };
+
+                CurrentConditions = new CurrentConditions()
+                {
+                    WeatherText = "Cloudy",
+                    Temperature = new Temperature()
+                    {
+                        Metric = new Units()
+                        {
+                            Value = "24"
+                        }
+                    }
+                };
+            }
+            SelectedCity = new City();
+            CurrentConditions = new CurrentConditions();
+            SearchCommand = new SearchCommand(this);
+            Cities = new ObservableCollection<City>();
+        }
+
+        private  void OnPropertyChange(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public async Task GetCurrentConditions()
+        {
+            //Query = string.Empty;
+            //Cities.Clear();
+            CurrentConditions = await AccuWeatherHelper.GetCurrentWeather(_selectedCity.Key);
+
+        }
+
+        public async Task MakeQuery()
+        {
+            var cities = await AccuWeatherHelper.GetCities(_query);
+            Cities.Clear();
+
+            foreach (var city in cities)
+            {
+                Cities.Add(city);
+            }
+        }
+
+    }
+}
